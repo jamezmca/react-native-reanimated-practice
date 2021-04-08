460feb27ba20f8a04a661c531857653da1f1e2e8
James McArthur <jamesmca@live.com>
2021-04-08 13:17:48 -0700

    putting a pin in it
    in search for the best drag and drop

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+const { width } = Dimensions.get('window');
+
+const { cond, eq, add, call, set, Value, event } = Animated;
 
 export default function App() {
+  const [dragX, setDragX] = useState(0)
+  const [dragY, setDragY] = useState(0)
+  const [offsetX, setOffsetX] = useState(width / 2)
+  const [offsetY, setOffsetY] = useState(100)
+  const [gestureState, setGestureState] = useState(-1)
+  const [onGestureEvent, setOnGestureEvent] = useState(event([{
+    nativeEvent: {
+      translationX: dragX,
+      translationY: dragY,
+      state: gestureState
+    }
+  }]))
+  const [addY, setAddY] = useState(add(offsetY, dragY))
+  const [addX, setAddX] = useState(add(offsetX, dragX))
+  const [transX, setTransX] = useState(cond(
+    eq(gestureState, State.ACTIVE),
+    addX,
+    set(offsetX, addX))
+    )
+  const [] = useState()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.Code>
+        {() =>
+          cond(
+            eq(this.gestureState, State.END),
+            call([this.addX, this.addY], this.onDrop)
+          )
+        }
+      </Animated.Code>
+      <PanGestureHandler
+        maxPointers={1}
+        minDist={10}
+        onGestureEvent={this.onGestureEvent}
+        onHandlerStateChange={this.onGestureEvent}>
+        <Animated.View
+          style={[
+            styles.box,
+            {
+              transform: [
+                {
+                  translateX: this.transX,
+                },
+                {
+                  translateY: this.transY,
+                },
+              ],
+            },
+          ]}
+        />
+      </PanGestureHandler>
     </View>
   );
 }
